d1aac5aa7d13533a522a1dcde613cc51ca628634
Paweł Trojański <80624809+enviGit@users.noreply.github.com>
2023-03-21 15:08:02 +0100

    Hovering effect added

--- script.js
+++ script.js
@@ -161,3 +161,19 @@ function audioPlayer() {
 }
 
 audioPlayer();
+
+//Hover Effect
+const handleOnMouseMove = e => {
+    const { currentTarget: target } = e;
+
+    const rect = target.getBoundingClientRect(),
+        x = e.clientX - rect.left,
+        y = e.clientY - rect.top;
+
+    target.style.setProperty("--mouse-x", `${x}px`);
+    target.style.setProperty("--mouse-y", `${y}px`);
+}
+
+for (const card of document.querySelectorAll(".project-info, .project-img, .project-img2")) {
+    card.onmousemove = e => handleOnMouseMove(e);
+}
